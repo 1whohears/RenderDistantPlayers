576012cde8dda9b2a021f3dd4fa6ff94a5be92dd
1whohears <1whohears28@gmail.com>
2025-03-31 21:21:20 -0500

    filter for only players in tracking events

--- src/main/java/com/onewhohears/distant_players/common/event/handler/DPCommonForgeEvents.java
+++ src/main/java/com/onewhohears/distant_players/common/event/handler/DPCommonForgeEvents.java
@@ -1,6 +1,7 @@
 package com.onewhohears.distant_players.common.event.handler;
 
 import com.onewhohears.distant_players.DistantPlayersMod;
+import com.onewhohears.onewholibs.util.UtilEntity;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -11,12 +12,24 @@ public class DPCommonForgeEvents {
 
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public static void startTrackingEvent(PlayerEvent.StartTracking event) {
+        if (!UtilEntity.isPlayer(event.getTarget())) return;
         System.out.println("player "+event.getEntity()+" start tracking "+event.getTarget());
     }
 
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public static void stopTrackingEvent(PlayerEvent.StopTracking event) {
+        if (!UtilEntity.isPlayer(event.getTarget())) return;
         System.out.println("player "+event.getEntity()+" stop tracking "+event.getTarget());
+    }
+
+    @SubscribeEvent(priority = EventPriority.NORMAL)
+    public static void playerLogIn(PlayerEvent.PlayerLoggedInEvent event) {
+
+    }
+
+    @SubscribeEvent(priority = EventPriority.NORMAL)
+    public static void playerLogOut(PlayerEvent.PlayerLoggedOutEvent event) {
+
     }
 
 }

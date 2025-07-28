a804819eb71c33e4965297cd68c3cfdf2381b4b9
1whohears <79810154+1whohears@users.noreply.github.com>
2025-07-28 01:13:21 -0500

    removed console spam

--- src/main/java/com/onewhohears/distant_players/client/core/DPClientManager.java
+++ src/main/java/com/onewhohears/distant_players/client/core/DPClientManager.java
@@ -82,7 +82,6 @@ public final class DPClientManager {
             );
 
             try {
-                System.out.println("rendering fake entity "+fake);
                 m.getEntityRenderDispatcher().render(
                         fake, d.x, d.y, d.z, f, partialTick, poseStack, buffer, packedLight
                 );

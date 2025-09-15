aa8ec19833df307270f1326f2d61ac6d0357c346
1whohears <79810154+1whohears@users.noreply.github.com>
2025-09-15 14:52:25 -0500

    update 1wholibs

--- common/src/main/java/com/onewhohears/distant_players/common/command/DPGameRules.java
+++ common/src/main/java/com/onewhohears/distant_players/common/command/DPGameRules.java
@@ -7,15 +7,13 @@ import net.minecraft.world.level.GameRules;
 public class DPGameRules {
     public static GameRules.Key<GameRules.IntegerValue> POS_UPDATE_RATE;
     public static GameRules.Key<GameRules.IntegerValue> CHECK_VISIBLE_RATE;
-    public static GameRules.Key<GameRules.IntegerValue> RAY_CAST_DEPTH;
     public static GameRules.Key<GameRules.IntegerValue> MAX_VIEW_DISTANCE;
     public static GameRules.Key<GameRules.IntegerValue> MAX_EXTRA_ENTITIES;
     public static GameRules.Key<GameRules.BooleanValue> TEST_MODE;
 
     public static void register() {
-        POS_UPDATE_RATE = CustomGameRules.registerInteger("distant_players:posUpdateRate", 5, GameRules.Category.PLAYER);
-        CHECK_VISIBLE_RATE = CustomGameRules.registerInteger("distant_players:checkVisibleRate", 20, GameRules.Category.PLAYER);
-        RAY_CAST_DEPTH = CustomGameRules.registerInteger("distant_players:rayCastDepth", 320, GameRules.Category.PLAYER);
+        POS_UPDATE_RATE = CustomGameRules.registerInteger("distant_players:posUpdateRate", 4, GameRules.Category.PLAYER);
+        CHECK_VISIBLE_RATE = CustomGameRules.registerInteger("distant_players:checkVisibleRate", 5, GameRules.Category.PLAYER);
         MAX_VIEW_DISTANCE = CustomGameRules.registerInteger("distant_players:maxViewDistance", 1000, GameRules.Category.PLAYER);
         MAX_EXTRA_ENTITIES = CustomGameRules.registerInteger("distant_players:maxExtraEntities", 50, GameRules.Category.PLAYER);
         TEST_MODE = CustomGameRules.registerBoolean("distant_players:testMode", false, GameRules.Category.PLAYER);
@@ -27,10 +25,6 @@ public class DPGameRules {
 
     public static int getCheckVisibleRate(MinecraftServer server) {
         return server.getGameRules().getInt(CHECK_VISIBLE_RATE);
-    }
-
-    public static int getRayCastDepth(MinecraftServer server) {
-        return server.getGameRules().getInt(RAY_CAST_DEPTH);
     }
 
     public static int getViewDistance(MinecraftServer server) {
